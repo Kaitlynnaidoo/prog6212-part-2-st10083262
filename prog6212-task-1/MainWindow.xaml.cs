756a5851ec0c965df6b087e8a27b0942d5aefc6d
lab_services_student <lab_services_student@lab000000>
2023-11-02 14:29:05 +0000

    Semester info window done.

--- prog6212-task-1/MainWindow.xaml.cs
+++ prog6212-task-1/MainWindow.xaml.cs
@@ -28,6 +28,43 @@ namespace prog6212_task_1
         private void submit_UI_Click(object sender, RoutedEventArgs e)
         {
 
+            // Indicates if one or more of the fields has an invalid value
+            bool errorFlag = false;
+            // Remove all errors from the previous button press
+            error_UI.Text = "";
+
+            // Get the values from the UI
+            var startDate = startDate_UI.SelectedDate;
+            var numWeeks = numWeeks_UI.Text;
+
+
+            // Validate all the values
+
+            if (startDate == null) // If the date in the date picker has not been set
+            {
+                errorFlag = true;
+                error_UI.Text += "The start date for the semester inputted is incorrect! ";
+            }
+
+            int numberOfWeeks = 0;
+            try
+            {
+                numberOfWeeks = Int32.Parse(numWeeks);
+            }
+            catch (FormatException)
+            {
+                errorFlag = true;
+                error_UI.Text += "The number of weeks inputted for the semester is incorrect! ";
+            }
+
+
+            if (!errorFlag) // If there are no errors
+            {
+                // Launch next window
+                /*ModulesWindow win = new ModulesWindow(weeks);
+                win.Show();
+                this.Close();*/
+            }
         }
     }
 }
